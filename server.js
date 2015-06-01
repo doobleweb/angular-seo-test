38fa26032d616245ea615d75a4a88417d2763e46
Asaf Katz <asf.ktz@gmail.com>
2015-06-01 14:30:24 +0300

    seo

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('port', process.env.PORT || 3000);
-
 app.use(express.static(__dirname + '/public'));
 
 app.all('/*', function(req, res, next) {
@@ -11,6 +9,7 @@ app.all('/*', function(req, res, next) {
     res.sendFile('public/index.html', { root: __dirname });
 });
 
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
     console.log('listen on port ' + app.get('port'));
 });
